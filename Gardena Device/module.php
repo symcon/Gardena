42beb171f1c4b50b5259fb4a5de80d1ec65a4945
TillBrede <51406030+TillBrede@users.noreply.github.com>
2021-03-19 13:57:16 +0100

    fixed device locale

--- Gardena Device/module.php
+++ Gardena Device/module.php
@@ -15,24 +15,6 @@ class GardenaDevice extends IPSModule
 
         //Properties
         $this->RegisterPropertyString('ID', '');
-
-        //Profiles
-
-        //Info about values from https://mips2648.github.io/jeedom-plugins-docs/gardena/en_US/
-        if (!IPS_VariableProfileExists('Gardena.State')) {
-            IPS_CreateVariableProfile('Gardena.State', VARIABLETYPE_STRING);
-            IPS_SetVariableProfileAssociation('Gardena.State', 'OK', $this->Translate('ok'), '', 0x00ff00);
-            IPS_SetVariableProfileAssociation('Gardena.State', 'WARNING', $this->Translate('warning'), '', 0xffff00);
-            IPS_SetVariableProfileAssociation('Gardena.State', 'UNAVAILABLE', $this->Translate('unavailable'), '', 0xff0000);
-            IPS_SetVariableProfileAssociation('Gardena.State', 'ERROR', $this->Translate('error'), '', 0xff0000);
-        }
-
-        //Might be different for each device
-        if (!IPS_VariableProfileExists('Gardena.Error')) {
-            IPS_CreateVariableProfile('Gardena.Error', VARIABLETYPE_STRING);
-            IPS_SetVariableProfileAssociation('Gardena.Error', 'NO_MESSAGE', $this->Translate('no message'), '', 0x00ff00);
-            IPS_SetVariableProfileAssociation('Gardena.Error', 'OFF_DISABLED', $this->Translate('off'), '', -1);
-        }
     }
 
     public function Destroy()
